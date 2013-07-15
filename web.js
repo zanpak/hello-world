b9cfefd881173d7b3dcc2f654127e40f5b4a62bc
zanpak <arash.toyser@gmail.com>
2013-07-15 18:38:35 +0200

    Web.js

---
+++ web.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var fs = require('fs');
+var app = express.createServer(express.logger());
+
+var buf = new Buffer(fs.readFileSync('index.html'));
+app.get('/', function(request, response) {
+  response.send(buf.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
